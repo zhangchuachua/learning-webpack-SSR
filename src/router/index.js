6231885ebe8f3f4259bcbc4bd9b1dee092f2c439
zhangchuachua <1097341044@qq.com>
2023-11-06 18:53:30 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,15 @@ import ssrRouter from "./ssr";
 
 const router = new Router();
 
-router.use('/ssr', ssrRouter.routes())
+router.use("/ssr", ssrRouter.routes());
+
+router.get("/download/buhounlocker.dmg", async (ctx) => {
+  const currentTime = Date.now();
+  while (Date.now() - currentTime < 1000) {}
+
+  ctx.body = {
+    success: true,
+  };
+});
 
 export default router;
